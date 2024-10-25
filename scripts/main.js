5af9481fb08253fcea5c4c4b7f63e04ba3463260
chlsey <chelseywang0219@gmail.com>
2024-10-25 17:23:10 -0400

    changing approach to better integrate different sorting methods

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
+var inp_as=document.getElementById('array-size'),array_size=inp_as.value;
+var inp_gen=document.getElementById("a_generate");
+var array_speed=document.getElementById('a_speed').value;
+
